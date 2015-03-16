cf33121851148ce5f9fcd15322852fe04ffcabf3
andrew <andporter@gmail.com>
2015-03-16 12:51:23 -0600

    updated the chart views when the data is changed

--- Stocks/WebContent/js/Stocks.js
+++ Stocks/WebContent/js/Stocks.js
@@ -1,27 +1,36 @@
 function piechart(stock1, stock2, stock3, stock1value, stock2value, stock3value, remainder) 
 {	
+	console.log("piechart")
+	console.log("stock1value: " + stock1value);
+	console.log("stock2value: " + stock2value);
+	console.log("stock3value: " + stock3value);
+	console.log("remainder: " + remainder);
+	
+	
 	google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(drawChart);
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
+
+    function drawPieChart() {
+      var Piechartdata = null;
+      Piechartdata = new google.visualization.arrayToDataTable([
         ['Stock', 		'Price'],
-        [stock1,     	stock1value],
-        [stock2,      	stock2value],
-        [stock3,  		stock3value],
-        ['Remainder',  	remainder]
+        [stock1,     	parseFloat(stock1value)],
+        [stock2,      	parseFloat(stock2value)],
+        [stock3,  		parseFloat(stock3value)],
+        ['Remainder',  	parseFloat(remainder)]
       ]);
 
-      var options = {
+      var Piechartoptions = {
         title: ''
       };
       
-      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+      var Piechart = null;
+      Piechart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-      function selectHandler() {
-          var selectedItem = chart.getSelection()[0];
+      function PiechartselectHandler() {
+          var selectedItem = Piechart.getSelection()[0];
           if (selectedItem) {
-            var stock = data.getValue(selectedItem.row, 0);
-            var value = data.getValue(selectedItem.row, 1);
+            var stock = Piechartdata.getValue(selectedItem.row, 0);
+            var value = Piechartdata.getValue(selectedItem.row, 1);
             if(stock == stock1)
             	changeValue(stock,1);
             else if(stock == stock2)
@@ -31,39 +40,48 @@ function piechart(stock1, stock2, stock3, stock1value, stock2value, stock3value,
           }
         }
       
-      google.visualization.events.addListener(chart, 'select', selectHandler); 
-      	
-      chart.draw(data, options);
+      google.visualization.events.addListener(Piechart, 'select', PiechartselectHandler); 
+      Piechart.draw(Piechartdata, Piechartoptions);
     }
+    drawPieChart();
 }
 
 function barchart(stock1, stock2, stock3, stock1value, stock2value, stock3value, remainder) 
 {
+	console.log("barchart")
+	console.log("stock1value: " + stock1value);
+	console.log("stock2value: " + stock2value);
+	console.log("stock3value: " + stock3value);
+	console.log("remainder: " + remainder);
+	
 	google.load("visualization", "1.1", {packages:["bar"]});
-    google.setOnLoadCallback(drawChart);
-    function drawChart() {
-      var data = google.visualization.arrayToDataTable([
+
+    function drawBarChart() 
+    {
+      var Barchartdata = null;
+      Barchartdata = new google.visualization.arrayToDataTable([
         ['Stock', 		'Price'],
-        [stock1,     	stock1value],
-        [stock2,      	stock2value],
-        [stock3,  		stock3value],
-        ['Remainder',  	remainder]
+        [stock1, parseFloat(stock1value)],
+        [stock2, parseFloat(stock2value)],
+        [stock3, parseFloat(stock3value)],
+        ['Remainder',  	parseFloat(remainder)]
       ]);
 
-      var options = {
+      var Barchartoptions = {
         chart: {
           title: ''
         },
         bars: 'vertical' // Required for Material Bar Charts.
       };
 
-      var chart = new google.charts.Bar(document.getElementById('barchart'));
+      var Barchart = null;
+      Barchart = new google.charts.Bar(document.getElementById('barchart'));
 
-      function selectHandler() {
-          var selectedItem = chart.getSelection()[0];
+      function BarchartselectHandler() {
+          var selectedItem = Barchart.getSelection()[0];
           if (selectedItem) {
-            var stock = data.getValue(selectedItem.row, 0);
-            var value = data.getValue(selectedItem.row, 1);
+            var stock = Barchartdata.getValue(selectedItem.row, 0);
+            var value = Barchartdata.getValue(selectedItem.row, 1);
             if(stock == stock1)
             	changeValue(stock,1);
             else if(stock == stock2)
@@ -73,80 +91,102 @@ function barchart(stock1, stock2, stock3, stock1value, stock2value, stock3value,
           }
         }
       
-      google.visualization.events.addListener(chart, 'select', selectHandler); 
+      google.visualization.events.addListener(Barchart, 'select', BarchartselectHandler); 
       	
-      chart.draw(data, options);
+      Barchart.draw(Barchartdata, Barchartoptions);
     }
+    
+    drawBarChart();
 }
 
 
 function changeValue(stock, chosenStock)
 {
+	
 	var stock1 = document.getElementById('stock1').value;
 	var stock2 = document.getElementById('stock2').value;
 	var stock3 = document.getElementById('stock3').value;
-	var pricePerStock1 = document.getElementById('stock1price').value;
-	var pricePerStock2 = document.getElementById('stock2price').value;
-	var pricePerStock3 = document.getElementById('stock3price').value;
-	var stockShares1 = document.getElementById('stock1shares').value;
-	var stockShares2 = document.getElementById('stock2shares').value;
-	var stockShares3 = document.getElementById('stock3shares').value;
-	var stockvalue1 = document.getElementById('stock1investment').value;
-	var stockvalue2 = document.getElementById('stock2investment').value;
-	var stockvalue3 = document.getElementById('stock3investment').value;
-	var rem = document.getElementById('investmentRemainder').value;
-	var investmentTot = document.getElementById('investmentTotal').value;
+	var stock1price = document.getElementById('stock1price').value;
+	var stock2price = document.getElementById('stock2price').value;
+	var stock3price = document.getElementById('stock3price').value;
+	var stock1shares = document.getElementById('stock1shares').value;
+	var stock2shares = document.getElementById('stock2shares').value;
+	var stock3shares = document.getElementById('stock3shares').value;
+	var stock1investment = document.getElementById('stock1investment').value;
+	var stock2investment = document.getElementById('stock2investment').value;
+	var stock3investment = document.getElementById('stock3investment').value;
+	var investmentRemainder = document.getElementById('investmentRemainder').value;
+	var investmentTotal = document.getElementById('investmentTotal').value;
 	
 	if(chosenStock == 1)
 	{
 		
 		//alert("Price of  " +  stock + " = " + pricePerStock1 + " and you own " + stock1 + " shares" + " and your their total worth is " + stockvalue1);
-		var newAmount = prompt('Please enter the new amount:  \n'+ 'Stock = '+ stock + ', Value = ' + stockvalue1 + ', Shares =  ' + stockShares1,stockShares1);
+		var newAmount = prompt('Please enter the new amount:  \n'+ 'Stock = '+ stock + ', Value = ' + stock1investment + ', Shares =  ' + stock1shares,stock1shares);
 		if(newAmount)
 			{
-				var newTotal = parseFloat(Math.round((pricePerStock1 * newAmount)*100.00)/100.00).toFixed(2);
+				var newTotal = parseFloat(Math.round((stock1price * newAmount)*100.00)/100.00).toFixed(2);
 				
 				document.getElementById("stock1shares").value=newAmount; 
 				document.getElementById("stock1investment").value=newTotal;
 				
-				
-				
-				piechart(stock,stock2,stock3,newTotal,stockvalue2,stockvalue3,rem);
-				barchart(stock,stock2,stock3,newTotal,stockvalue2,stockvalue3,rem);
-				
-				alert(stock+", "+stock2 + ", "+stock3+", "+newTotal+", "+stockvalue2+", "+stockvalue3 +", " +rem);
+				//alert(stock1+", "+stock2 + ", "+stock3+", "+newTotal+", "+stockvalue2+", "+stockvalue3 +", " +rem);
 						
-				
+				updateCharts();
 			}
 		
 	}
 	else if(chosenStock == 2)
 	{
-		var newAmount = prompt('Please enter the new amount:  \n'+ 'Stock = '+ stock + ', Value = ' + stockvalue2 + ', Shares =  ' + stockShares2,stockShares2);
+		var newAmount = prompt('Please enter the new amount:  \n'+ 'Stock = '+ stock + ', Value = ' + stock2investment + ', Shares =  ' + stock2shares,stock2shares);
 		if(newAmount)
 		{
-			var newTotal = parseFloat(Math.round((pricePerStock2 * newAmount)*100.00)/100.00).toFixed(2);
+			var newTotal = parseFloat(Math.round((stock2price * newAmount)*100.00)/100.00).toFixed(2);
 			document.getElementById("stock2shares").value=newAmount; 
 			document.getElementById("stock2investment").value=newTotal;
 		
-			//piechart(stock1, stock, stock3, stockvalue1, newTotal, stockvalue3, rem);
-			//barchart(stock1, stock, stock3, stockvalue1, newTotal, stockvalue3, rem);
+			updateCharts();
 		}
 	}
 	else if(chosenStock == 3)
 	{
-		var newAmount = prompt('Please enter the new amount:  \n'+ 'Stock = '+ stock + ', Value = ' + stockvalue3 + ', Shares =  ' + stockShares3,stockShares3);
+		var newAmount = prompt('Please enter the new amount:  \n'+ 'Stock = '+ stock + ', Value = ' + stock3investment + ', Shares =  ' + stock3shares,stock3shares);
 		if(newAmount)
 		{
-			var newTotal = parseFloat(Math.round((pricePerStock3 * newAmount)*100.00)/100.00).toFixed(2);
+			var newTotal = parseFloat(Math.round((stock3price * newAmount)*100.00)/100.00).toFixed(2);
 			document.getElementById("stock3shares").value=newAmount; 
 			document.getElementById("stock3investment").value=newTotal;
 			
-			//piechart(stock1, stock2, stock, stockvalue1, stockvalue2, newTotal, rem);
-			//barchart(stock1, stock2, stock, stockvalue1, stockvalue2, newTotal, rem);
+			updateCharts();
 		}
 	}
 		
+}
+
+function updateCharts() 
+{
+	var stock1 = document.getElementById('stock1').value;
+	var stock2 = document.getElementById('stock2').value;
+	var stock3 = document.getElementById('stock3').value;
+	var stock1price = document.getElementById('stock1price').value;
+	var stock2price = document.getElementById('stock2price').value;
+	var stock3price = document.getElementById('stock3price').value;
+	var stock1shares = document.getElementById('stock1shares').value;
+	var stock2shares = document.getElementById('stock2shares').value;
+	var stock3shares = document.getElementById('stock3shares').value;
+	var stock1investment = document.getElementById('stock1investment').value;
+	var stock2investment = document.getElementById('stock2investment').value;
+	var stock3investment = document.getElementById('stock3investment').value;
+	var investmentRemainder = document.getElementById('investmentRemainder').value;
+	var investmentTotal = document.getElementById('investmentTotal').value;
+	
+	console.log("stock1investment: " + stock1investment);
+	console.log("stock2investment: " + stock2investment);
+	console.log("stock3investment: " + stock3investment);
+	console.log("investmentRemainder: " + investmentRemainder);
+	
+	piechart(stock1, stock2, stock3, stock1investment, stock2investment, stock3investment, investmentRemainder);
+	barchart(stock1, stock2, stock3, stock1investment, stock2investment, stock3investment, investmentRemainder);
 }
 
 
